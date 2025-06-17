eba528ee0bada1da240f387e6e4a99a103d74d6a
caye <caye1300@gmail.com>
2025-06-16 23:41:08 -0700

    fixed npm build issue

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -1,9 +1,32 @@
+// import Link from "next/link"
+// import { ArrowLeftIcon } from "@heroicons/react/24/solid"
+// import { getArticleData } from "@/lib/articles"
+
+// const Article = async ({ params }: { params: { slug: string } }) => {
+//     const articleData = await getArticleData(params.slug)
+
+//     return (
+//         <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
+//             <div className="flex justify-between font-sourceSans3">
+//                 <Link href={"/notes"} className="articleLinks flex flex-row gap-1 place-items-center hover:text-green-800 transition duration-150 ">
+//                     <ArrowLeftIcon width={20} />
+//                     <p>back to home</p>
+//                 </Link>
+//                 <p>{articleData.date.toString()}</p>
+//             </div>
+//             <article className="article" dangerouslySetInnerHTML={{ __html: articleData.contentHtml }} />
+//         </section>
+//     )
+// }
+
+// export default Article
 import Link from "next/link"
 import { ArrowLeftIcon } from "@heroicons/react/24/solid"
 import { getArticleData } from "@/lib/articles"
 
-const Article = async ({ params }: { params: { slug: string } }) => {
-    const articleData = await getArticleData(params.slug)
+const Article = async ({ params }: { params: Promise<{ slug: string }> }) => {
+    const { slug } = await params
+    const articleData = await getArticleData(slug)
 
     return (
         <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
