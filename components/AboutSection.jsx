9af8e7c818e6fdd63b675e9a4a8b8ddb8a9bb815
caye <caye1300@gmail.com>
2025-08-20 12:28:42 -0700

    ml skills

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -22,7 +22,7 @@ const TAB_DATA = [
           Database Management: MySQL, MongoDB, PostgreSQL, Prisma
         </li>
         <li className="text-red-950 leading-relaxed text-lg font-350 list-disc">
-          AI/ML: Scikit-learn, TensorFlow, PyTorch, Jupyter Notebooks, Keras, NumPy, Pandas, Matplotlib, OpenCV, OpenAI API, LangChain, Hugging Face
+          AI/ML: Scikit-learn, TensorFlow, PyTorch, Jupyter Notebooks, Keras, NumPy, Pandas, Matplotlib, OpenCV, Kafka, OpenAI API, LangChain, NLTK, Hugging Face
         </li>
         <li className="text-red-950 leading-relaxed text-lg font-350 list-disc">
            UI/UX: Figma, Tailwind CSS
